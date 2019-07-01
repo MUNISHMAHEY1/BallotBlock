799e24af97cb4744a92a2fead0f0f1c0873ad2e1
Fabio Santiago <fabio.ribeiro.santiago@gmail.com>
2019-07-01 12:04:17 -0300

    Closes #2

--- election/views.py
+++ election/views.py
@@ -13,11 +13,13 @@ from election.forms import VoteForm
 @staff_member_required
 def config_mock_election(request, elector_quantity=500, template_name='mock_election.html'):
 
+    # Clean database
     Elector.objects.all().delete()
     Candidate.objects.all().delete()
     Position.objects.all().delete()
     User.objects.filter(is_superuser=False).delete()
 
+    # Create 1st position
     position = Position.objects.create(description="Best soccer player of all time")
     position.save()
 
@@ -26,6 +28,7 @@ def config_mock_election(request, elector_quantity=500, template_name='mock_elec
         c = Candidate.objects.create(position=position, name=name)
         c.save()
 
+    # Create 2nd position
     position = Position.objects.create(description="Best city to live in Canada")
     position.save()
 
@@ -34,7 +37,11 @@ def config_mock_election(request, elector_quantity=500, template_name='mock_elec
         c = Candidate.objects.create(position=position, name=name)
         c.save()
     
-    
+    '''
+    # Create "elector_quantity" of electors 
+    # username: userXXX
+    # pass: BalletBlockXXX where XXX is ther user number from 001 to elector_quantity
+    '''
     i = 1
     s = len(str(elector_quantity))
     while i < elector_quantity:
