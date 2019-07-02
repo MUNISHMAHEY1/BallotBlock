88801dcc0dde91fa2fcfcad08fbf24a1e7f45d02
Fabio Santiago <fabio.ribeiro.santiago@gmail.com>
2019-07-02 12:11:48 -0300

    closes #7

---
+++ chain/business.py
@@ -0,0 +1,90 @@
+from django.core.serializers.json import DjangoJSONEncoder
+from election.models import ElectionConfig, Candidate, Elector, Position
+from django.contrib.auth.models import User
+import hashlib
+import json
+import os
+from balletblock import settings
+
+
+class HashCalculator():
+
+    def databaseHash(self):
+        hash_dict = {}
+
+        # Include hash for Election Config
+        queryset = ElectionConfig.objects.all()
+        cs_json_str = json.dumps(list(queryset.values()), cls=DjangoJSONEncoder)
+        m = hashlib.sha512()
+        m.update(cs_json_str.encode("utf-8"))
+        hash_dict['election_config'] = m.hexdigest()
+
+        # Include hash for Candidates
+        queryset = Candidate.objects.all()
+        cs_json_str = json.dumps(list(queryset.values()), cls=DjangoJSONEncoder)
+        m = hashlib.sha512()
+        m.update(cs_json_str.encode("utf-8"))
+        hash_dict['candidates'] = m.hexdigest()
+
+        # Include hash for Positions
+        queryset = Position.objects.all()
+        cs_json_str = json.dumps(list(queryset.values()), cls=DjangoJSONEncoder)
+        m = hashlib.sha512()
+        m.update(cs_json_str.encode("utf-8"))
+        hash_dict['positions'] = m.hexdigest()
+
+        # Include hash for Electors
+        queryset = Position.objects.all()
+        cs_json_str = json.dumps(list(queryset.values()), cls=DjangoJSONEncoder)
+        m = hashlib.sha512()
+        m.update(cs_json_str.encode("utf-8"))
+        hash_dict['electors'] = m.hexdigest()
+
+        # Include hash for Users
+        queryset = User.objects.all()
+        cs_json_str = json.dumps(list(queryset.values()), cls=DjangoJSONEncoder)
+        m = hashlib.sha512()
+        m.update(cs_json_str.encode("utf-8"))
+        hash_dict['users'] = m.hexdigest()
+
+        return {'database':hash_dict}
+
+    def __ignoreFile(self, filename):
+        if filename.startswith('.'):
+            return True
+        if filename.endswith('.pyc'):
+            print(filename)
+            return True
+        return False
+
+    def sourceCodeHash(self):
+        BUF_SIZE = 65536
+        hash_dict = {}
+
+        for root, dirs, files in os.walk(settings.BASE_DIR):
+            for filename in files:
+                if not os.path.isdir(os.path.join(root, filename)):
+                    if not self.__ignoreFile(str(filename)):
+                        m = hashlib.sha512()
+                        with open(os.path.join(root, filename), 'rb') as f:
+                            while True:
+                                data = f.read(BUF_SIZE)
+                                if not data:
+                                    break
+                                m.update(data)
+                        hash_dict[os.path.join(root, filename)] = m.hexdigest()
+
+        '''
+        for filename in os.listdir(settings.BASE_DIR):
+            m = hashlib.sha512()
+            if not os.path.isdir(filename):
+                with open(filename, 'rb') as f:
+                    while True:
+                        data = f.read(BUF_SIZE)
+                        if not data:
+                            break
+                        m.update(data)
+            hash_dict[os.path.abspath(filename)] = m.hexdigest()
+        '''
+
+        return {'souce_code':hash_dict}
