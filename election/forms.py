3655d1fc2a9749cdd984e741a50a3e6d61d5d63c
Fabio Santiago <fabio.ribeiro.santiago@gmail.com>
2019-07-05 15:25:00 -0300

    Closes #14. Add a navigation bar for each html

--- election/forms.py
+++ election/forms.py
@@ -27,22 +27,18 @@ class ElectionConfigForm(forms.ModelForm):
         self.helper.form_tag = False
         self.helper.form_method = 'post'
         self.helper.layout = Layout(
-            Fieldset(
-                'Configuration Data',
-                Row(
-                    Column('description', css_class='form-group col-md-4 mb-0'),
-                    Column('start_time', css_class='form-group col-md-4 mb-0'),
-                    Column('end_time', css_class='form-group col-md-4 mb-0'),
-                    Column('block_time_generation', css_class='form-group col-md-4 mb-0'),
-                    Column('guess_rate', css_class='form-group col-md-4 mb-0'),
-                    Column('min_votes_in_block', css_class='form-group col-md-4 mb-0'),
-                    Column('min_votes_in_last_block', css_class='form-group col-md-4 mb-0'),
-                    Column('attendance_rate', css_class='form-group col-md-4 mb-0'),
-                    Column('locked', css_class='form-group col-md-4 mb-0'),
-                    #css_class='form-row'
-                ),
+            Row(
+                Column('description', css_class='form-group col-md-4 mb-0'),
+                Column('start_time', css_class='form-group col-md-4 mb-0'),
+                Column('end_time', css_class='form-group col-md-4 mb-0'),
+                Column('block_time_generation', css_class='form-group col-md-4 mb-0'),
+                Column('guess_rate', css_class='form-group col-md-4 mb-0'),
+                Column('min_votes_in_block', css_class='form-group col-md-4 mb-0'),
+                Column('min_votes_in_last_block', css_class='form-group col-md-4 mb-0'),
+                Column('attendance_rate', css_class='form-group col-md-4 mb-0'),
+                Column('locked', css_class='form-group col-md-4 mb-0'),
+                #css_class='form-row'
             ),
-
         )
         
     '''
@@ -94,21 +90,16 @@ class electionconfigviewForm(forms.ModelForm):
         self.helper.form_tag = False
         self.helper.form_method = 'post'
         self.helper.layout = Layout(
-            Fieldset(
-                'Configuration Data',
-                Row(
-                    Column('description', css_class='form-group col-md-4 mb-0'),
-                    Column('start_time', css_class='form-group col-md-4 mb-0'),
-                    Column('end_time', css_class='form-group col-md-4 mb-0'),
-                    Column('block_time_generation', css_class='form-group col-md-4 mb-0'),
-                    Column('guess_rate', css_class='form-group col-md-4 mb-0'),
-                    Column('min_votes_in_block', css_class='form-group col-md-4 mb-0'),
-                    Column('min_votes_in_last_block', css_class='form-group col-md-4 mb-0'),
-                    Column('attendance_rate', css_class='form-group col-md-4 mb-0'),
-
-                ),
+            Row(
+                Column('description', css_class='form-group col-md-4 mb-0'),
+                Column('start_time', css_class='form-group col-md-4 mb-0'),
+                Column('end_time', css_class='form-group col-md-4 mb-0'),
+                Column('block_time_generation', css_class='form-group col-md-4 mb-0'),
+                Column('guess_rate', css_class='form-group col-md-4 mb-0'),
+                Column('min_votes_in_block', css_class='form-group col-md-4 mb-0'),
+                Column('min_votes_in_last_block', css_class='form-group col-md-4 mb-0'),
+                Column('attendance_rate', css_class='form-group col-md-4 mb-0'),
             ),
-
         )
 
 
