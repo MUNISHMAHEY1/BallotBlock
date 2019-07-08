02cdad7b14132f148631fd1c29972ef3381d3db1
Fabio Santiago <fabio.ribeiro.santiago@gmail.com>
2019-07-08 15:13:14 -0300

    Update migration files and remove database from repository

--- vote/models.py
+++ vote/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from election.models import Elector, Candidate
 
 class Voted(models.Model):
-    elector = models.ForeignKey(Elector, blank=False, null=False, unique=True, on_delete=models.PROTECT)
+    elector = models.OneToOneField(Elector, blank=False, null=False, unique=True, on_delete=models.PROTECT)
     hash_val= models.CharField(max_length=200, null=True, blank=False, default=None)
 
 class CandidateVote(models.Model):
-    candidate = models.ForeignKey(Candidate, blank=False, null=False, unique=True, on_delete=models.PROTECT)
+    candidate = models.OneToOneField(Candidate, blank=False, null=False, unique=True, on_delete=models.PROTECT)
     quantity = models.IntegerField(null=False, blank=False, default=0)
