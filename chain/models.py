0e8375f82cd7baeaa342ebe7ad59abe3d0a8b066
thesarathpulikkal <sarathhari@gmail.com>
2019-07-11 17:59:43 -0400

    New db added to chain/models.py

--- chain/models.py
+++ chain/models.py
@@ -6,3 +6,14 @@ from django.db import models
 class BlockStructure(models.Model):
      BlockNo= models.IntegerField(unique=True,null=False,blank=False)
      ParentHash=models.CharField(max_length=200, null=True, blank=False, default=None)
+
+class BBlock(models.Model):
+     block_no=models.IntegerField(unique=True,null=False, blank=False)
+     db_hash=models.TextField(blank=False,unique=True)
+     sc_hash=models.TextField(blank=False,unique=True)
+     candidate_votes=models.TextField(blank=True,null=True)
+     candidate_votes=models.TextField(blank=True,null=True)
+     voters_voted=models.TextField(blank=True,null=True)
+     total_votes=models.IntegerField(blank=False)
+     parent_hash=models.TextField(blank=False,unique=True)
+     
