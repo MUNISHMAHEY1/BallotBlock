863bf9bb251a769ee681286419ff9cf1ee2be228
Fabio Santiago <fabio.ribeiro.santiago@gmail.com>
2019-07-22 19:11:48 -0300

    #34 Remove django admin content files

--- balletblock/urls.py
+++ balletblock/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from balletblock import views
 from django.urls import include
@@ -23,7 +22,6 @@ from balletblock import settings
 urlpatterns = [
     path('', views.home, name='home'),
     path('about_us/', views.about_us, name='about_us'),
-    path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('chain/', include('chain.urls')),
     path('election/', include('election.urls')),
