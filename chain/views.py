93f2dea65e577b68c20b325a683ef93168762315
Fabio Santiago <fabio.ribeiro.santiago@gmail.com>
2019-07-26 12:35:28 -0300

    Fix chain validation

--- chain/views.py
+++ chain/views.py
@@ -48,6 +48,7 @@ def source_code_hash(request, bblock_id, template_name='source_code_hash.html'):
 def validate_chain(request):
     valid = True
     parent_hash = '0'.zfill(128)
+    genesis_block = BBlock.objects.all().order_by('timestamp_iso')[0]
     for bblock in BBlock.objects.all().order_by('timestamp_iso'):
         if bblock.parent_hash != parent_hash:
             messages.error(request, mark_safe('Parent hash does not match<br>Block parent hash: {}<br>Parent hash: {}'.format(bblock.parent_hash, 
@@ -58,6 +59,14 @@ def validate_chain(request):
         if bblock.block_hash != calculated_hash:
             messages.error(request, mark_safe('Block hash does not match<br>Stored hash: {}<br>Calculated hash: {}'.format(bblock.block_hash, 
                                 calculated_hash)) )
+            valid = False
+        if bblock.hash_of_database_hash != genesis_block.hash_of_database_hash:
+            messages.error(request, mark_safe('Database hash does not match<br>Database hash of block: {}<br>Genesis Block database hash: {}'.format(bblock.hash_of_database_hash, 
+                                genesis_block.hash_of_database_hash)) )
+            valid = False
+        if bblock.hash_of_source_code_hash != genesis_block.hash_of_source_code_hash:
+            messages.error(request, mark_safe('Source code hash does not match<br>Source code hash of block: {}<br>Genesis Block source code hash: {}'.format(bblock.hash_of_source_code_hash, 
+                                genesis_block.hash_of_source_code_hash)) )
             valid = False
     if valid:
         messages.success(request, 'Chain is valid')
